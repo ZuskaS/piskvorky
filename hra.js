1d02ad32a7e00fd251d820ec1f6c7353aeabb6e6
Zuzana Ilečková <yes>
2025-05-17 22:39:24 +0200

    oprava, aby se kód spouštěl až po načtení stránky

--- hra.js
+++ hra.js
@@ -1,93 +1,95 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
-//nastavení hráče, který je na začátku na tahu - kolečko
-let currentPlayer = 'circle';
+document.addEventListener('DOMContentLoaded', () => {
+  //nastavení hráče, který je na začátku na tahu - kolečko
+  let currentPlayer = 'circle';
 
-//výběr všech políček herní plochy
-const boardFields = document.querySelectorAll('.tabulka .policko');
+  //výběr všech políček herní plochy
+  const boardFields = document.querySelectorAll('.tabulka .policko');
 
-//Element, kde se zobrazuje obrázek hráče, který je na tahu
-const imgCurrentPlayer = document.querySelector('.leftSide img');
+  //Element, kde se zobrazuje obrázek hráče, který je na tahu
+  const imgCurrentPlayer = document.querySelector('.leftSide img');
 
-//pomocná funkce pro kontrolu, zda hra skončila pro kontrolu před přidáním AI hráče
-// převod DOM políček do pole se znaky 'x', 'o', nebo '_' (prázdné)
-const getPlayBoard = () => {
-  return Array.from(boardFields).map((policko) => {
-    if (policko.classList.contains('board__field--cross')) return 'x';
-    if (policko.classList.contains('board__field--circle')) return 'o';
-    return '_';
-  });
-};
-console.log(getPlayBoard());
+  //pomocná funkce pro kontrolu, zda hra skončila pro kontrolu před přidáním AI hráče
+  // převod DOM políček do pole se znaky 'x', 'o', nebo '_' (prázdné)
+  const getPlayBoard = () => {
+    return Array.from(boardFields).map((policko) => {
+      if (policko.classList.contains('board__field--cross')) return 'x';
+      if (policko.classList.contains('board__field--circle')) return 'o';
+      return '_';
+    });
+  };
+  console.log(getPlayBoard());
 
-//zjišťění, zda někdo vyhrál a kdo, nebo je remíza
-const checkGameState = () => {
-  const winner = findWinner(getPlayBoard());
+  //zjišťění, zda někdo vyhrál a kdo, nebo je remíza
+  const checkGameState = () => {
+    const winner = findWinner(getPlayBoard());
 
-  if (winner === 'x' || winner === 'o') {
-    setTimeout(() => {
-      alert(`Vyhrál hráč se symbolem ${winner}.`);
+    if (winner === 'x' || winner === 'o') {
+      setTimeout(() => {
+        alert(`Vyhrál hráč se symbolem ${winner}.`);
+        window.location.reload();
+      }, 300);
+      return true;
+    }
+
+    if (winner === 'tie') {
+      alert(`Hra skončila nerozhodně.`);
       window.location.reload();
-    }, 300);
-    return true;
-  }
+      return true;
+    }
 
-  if (winner === 'tie') {
-    alert(`Hra skončila nerozhodně.`);
-    window.location.reload();
-    return true;
-  }
+    return false; //hra ještě neskončila
+  };
 
-  return false; //hra ještě neskončila
-};
-
-//pomocná funkce - kontrola, zda hra skončila, zda je na tahu křížek a zavolá API a odehraje AI tah tedy křížek
-const aiMove = async () => {
-  const response = await fetch(
-    'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
-    {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
+  //pomocná funkce - kontrola, zda hra skončila, zda je na tahu křížek a zavolá API a odehraje AI tah tedy křížek
+  const aiMove = async () => {
+    const response = await fetch(
+      'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+      {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          board: getPlayBoard(),
+          player: 'x',
+        }),
       },
-      body: JSON.stringify({
-        board: getPlayBoard(),
-        player: 'x',
-      }),
-    },
-  );
-  const data = await response.json();
-  console.log('Data z API:', data);
-  const { x, y } = data.position;
-  const index = x + y * 10;
-  console.log('Spočtený index:', index);
-  const targetField = boardFields[index]; // Najde políčko
-  console.log('Počet políček:', boardFields.length);
+    );
+    const data = await response.json();
+    console.log('Data z API:', data);
+    const { x, y } = data.position;
+    const index = x + y * 10;
+    console.log('Spočtený index:', index);
+    const targetField = boardFields[index]; // Najde políčko
+    console.log('Počet políček:', boardFields.length);
 
-  if (!targetField) {
-    console.error('Neexistující políčko na indexu:', index, data);
-    return;
-  }
-  targetField.click(); // Klikne na políčko, aby se provedl tah
-};
-
-//zaznamenání tahu hráče, střídání hráčů a aktualizace ikony hráče na tahu
-boardFields.forEach((policko) => {
-  policko.addEventListener('click', (event) => {
-    event.target.disabled = true;
-    if (currentPlayer === 'circle') {
-      event.target.classList.add('board__field--circle');
-      currentPlayer = 'cross';
-    } else {
-      event.target.classList.add('board__field--cross');
-      currentPlayer = 'circle';
+    if (!targetField) {
+      console.error('Neexistující políčko na indexu:', index, data);
+      return;
     }
+    targetField.click(); // Klikne na políčko, aby se provedl tah
+  };
 
-    // změna obrázku hráče na tahu
-    imgCurrentPlayer.src =
-      currentPlayer === 'circle' ? 'public/circle.svg' : 'public/cross.svg';
-    if (!checkGameState() && currentPlayer === 'cross') {
-      setTimeout(aiMove, 500);
-    }
+  //zaznamenání tahu hráče, střídání hráčů a aktualizace ikony hráče na tahu
+  boardFields.forEach((policko) => {
+    policko.addEventListener('click', (event) => {
+      event.target.disabled = true;
+      if (currentPlayer === 'circle') {
+        event.target.classList.add('board__field--circle');
+        currentPlayer = 'cross';
+      } else {
+        event.target.classList.add('board__field--cross');
+        currentPlayer = 'circle';
+      }
+
+      // změna obrázku hráče na tahu
+      imgCurrentPlayer.src =
+        currentPlayer === 'circle' ? 'public/circle.svg' : 'public/cross.svg';
+      if (!checkGameState() && currentPlayer === 'cross') {
+        setTimeout(aiMove, 500);
+      }
+    });
   });
 });
